d369ed00d103422cb4f4d0d073a171cbad4c33cc
바키찬 <ckstmznf@naver.com>
2024-01-13 01:45:48 +0900

    Input 컴포넌트

---
+++ src/component/Input.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const InputStyle = styled.input`
+  color: ${p => p.theme.color.Gray8};
+  font-size: 16px;
+  font-weight: 400;
+  
+  border: 1px solid ${p => p.theme.color.Gray4};
+  border-radius: 6px;
+  
+  padding: 12px;
+  background-color: ${props => props.theme.color.Gray1};
+  
+  &::placeholder {
+    font-weight: 500;
+    opacity: 0.7;
+  }
+`
+
+const Input = ({...rest}) => {
+  return (
+    <InputStyle {...rest}/>
+  )
+}
+
+export default Input
