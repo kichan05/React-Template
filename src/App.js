5d38f5ae88679dd4b7aadbafd316d7a8ca953d3d
바키찬 <ckstmznf@naver.com>
2024-01-13 01:45:34 +0900

    Resize 이벤트 분리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import {GlobalStyle, Theme} from "./style/GlobalStyle";
 import {ThemeProvider} from "styled-components";
-import {useEffect} from "react";
-import Button from "./component/Button";
+import {Resize} from "./component/Resize";
+import Input from "./component/Input";
 
 function App() {
-  const handleResize = () => {
-    const vh = window.innerHeight / 100;
-    document.documentElement.style.setProperty("--vh", `${vh}px`)
-  }
-  useEffect(() => {
-    handleResize()
-    window.addEventListener("resize", handleResize)
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  })
-
   return (
     <div className="App">
       <ThemeProvider theme={Theme}>
         <GlobalStyle/>
-        <Button background={"Indigo6"}>버튼</Button>
+        <Resize/>
+        <Input placeholder={"name"}/>
       </ThemeProvider>
     </div>
   );
--- src/component/Page.js
+++ src/component/Page.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const PageStyle = styled.div`
+  height: 100%;
   background-color: #f00;
   
   & > .content {
     width: 100%;
-    max-width: 1000px;
+    max-width: ${({theme}) => theme.size.mobileMaxWidth}px;
     
     margin: 0 auto;
-    
     background-color: #0f0;
   }
 `
@@ -17,7 +17,7 @@ const Page = () => {
   return (
     <PageStyle>
       <div className="content">
-
+        Hello
       </div>
     </PageStyle>
   )
